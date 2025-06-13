7629d7b7eedb6bd1cdc03c353c46f6c30b7057f1
Micael da Costa do Amaral <micael.amaral@antt.gov.br>
2025-06-13 12:21:24 -0300

    novas funcoes de limpar arquivos

--- temp_clean.bat
+++ temp_clean.bat
@@ -19,14 +19,14 @@ call :etapa "Limpando TEMP do usuario..."
 del /q /f /s "%TEMP%\*" 2>nul && (
   	call :ok "TEMP do usuario limpo."
 ) || (
-  	call :err "ERRO ao limpar TEMP!!!"
+  	call :err "!ERRO ao limpar TEMP!"
 )
 
 call :etapa "Limpando cache do Chrome..."
 del /q /f /s "%LOCALAPPDATA%\Google\Chrome\User Data\Default\Cache\*" 2>nul && (
   	call :ok "Cache do Chrome limpo."
 ) || (
-  	call :err "ERRO ao limpar cache do Chrome!!!"
+  	call :err "!ERRO ao limpar cache do Chrome!"
 )
 
 call :etapa "Limpando prefetch..."
@@ -51,17 +51,33 @@ del /q /f /s "%LOCALAPPDATA%\Microsoft\Windows\Explorer\thumbcache_*.db" 2>nul &
 )
 
 call :etapa "Limpando logs do Windows..."
-PowerShell.exe -NoProfile -Command "Clear-EventLog -Log *" 2>nul && (
+powershell -NoProfile -Command "Clear-EventLog -Log *" 2>nul && (
   	call :ok "Logs do Windows limpos."
 ) || (
   	call :err "ERRO ao limpar logs! Verifique permissao de ADM!"
 )
 
 call :etapa "Esvaziando lixeira..."
-PowerShell.exe -NoProfile -Command "Clear-RecycleBin -Force -ErrorAction SilentlyContinue" && (
+powershell -NoProfile -Command "Clear-RecycleBin -Force -ErrorAction SilentlyContinue" && (
 	call :ok "Lixeira esvaziada."
 ) || (
     	call :err "ERRO ao esvaziar lixeira."
+)
+
+call :etapa "Limpando standby memory (cache de RAM)..."
+powershell -NoProfile -Comand ^
+	"Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class PInvoke { [DllImport(\"ntdll.dll")] satic extern unit NtSetSystemInformation(int InfoClass, ref int Info, in Length); public static void ClearStandby(){int val=4; NtSetSystemInformation(0x0050, ref val, 4);} }';[PInvoke]::ClearStandby()" 2>nul && (
+	call : ok "Cache de RAM (standby memory) limpa."
+) || (
+	call :err "ERRO ao limpar Cache de RAM (standby memory)! Verifique permissao de ADM!"
+)
+
+call :etapa "Encerrando processos pesados..."
+powershell -NoProfile -Command ^
+  "Get-Process | Where-Object { $_.WorkingSet -gt 512MB -and $_.ProcessName -notin 'explorer','svchost','System','cmd' } | Stop-Process -Force" 2>nul && (
+    call :ok "Processos pesados encerrados."
+) || (
+    call :err "Falha ao encerrar processos pesados! Verifique permissao de ADM!"
 )
 
 echo.
